d784e0eabc4934c49797304db784f05723b3dba1
ttaerrim <ltr0121@naver.com>
2022-02-02 17:45:08 +0900

    Modify: ImageContainer CSS 수정

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,15 +1,18 @@
 import { useHomeState } from "HomeContext";
 import React from "react";
-
+import styled from "styled-components";
+const Image = styled.img`
+  width: 500px;
+`;
 const ImageContainer = () => {
   const state = useHomeState();
 
   const { data: home } = state.home;
 
   return (
-    <>
-      <img src={home && home.imageUrl} />
-    </>
+    <React.Fragment>
+      <Image src={home && home.imageUrl} />
+    </React.Fragment>
   );
 };
 
