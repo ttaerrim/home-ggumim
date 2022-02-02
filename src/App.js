e1a8cc93b2622e1e4976addf4bdf257b47445cbf
ttaerrim <ltr0121@naver.com>
2022-02-02 17:44:09 +0900

    Add: FurnitureIdx Provider 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import { HomeProvider } from "HomeContext";
+import { FurnitureProvider, HomeProvider } from "HomeContext";
 import HomeTemplate from "components/HomeTemplate";
 import FurnitureList from "components/FurnitureList";
 import ImageContainer from "components/ImageContainer";
 import ButtonList from "components/ButtonList";
+import GlobalStyle from "GlobalStyle";
 function App() {
   return (
-    <HomeProvider>
-      <HomeTemplate>
-        <ImageContainer />
-        <ButtonList />
-        <FurnitureList />
-      </HomeTemplate>
-    </HomeProvider>
+    <>
+      <GlobalStyle />
+      <HomeProvider>
+        <HomeTemplate>
+          <FurnitureProvider>
+            <ImageContainer />
+            <ButtonList />
+            <FurnitureList />
+          </FurnitureProvider>
+        </HomeTemplate>
+      </HomeProvider>
+    </>
   );
 }
 
--- src/HomeContext.js
+++ src/HomeContext.js
@@ -1,7 +1,6 @@
-import React, { createContext, useReducer, useContext } from "react";
+import React, { createContext, useReducer, useContext, useState } from "react";
 import axios from "axios";
 
-// UsersContext 에서 사용 할 기본 상태
 const initialState = {
   home: {
     loading: false,
@@ -10,28 +9,24 @@ const initialState = {
   },
 };
 
-// 로딩중일 때 바뀔 상태 객체
 const loadingState = {
   loading: true,
   data: null,
   error: null,
 };
 
-// 성공했을 때의 상태 만들어주는 함수
 const success = (data) => ({
   loading: false,
   data,
   error: null,
 });
 
-// 실패했을 때의 상태 만들어주는 함수
 const error = (error) => ({
   loading: false,
   data: null,
   error: error,
 });
 
-// 위에서 만든 객체 / 유틸 함수들을 사용하여 리듀서 작성
 function homeReducer(state, action) {
   switch (action.type) {
     case "GET_HOME":
@@ -54,14 +49,12 @@ function homeReducer(state, action) {
   }
 }
 
-// State 용 Context 와 Dispatch 용 Context 따로 만들어주기
 const HomeStateContext = createContext(null);
 const HomeDispatchContext = createContext(null);
+const FurnitureIdxContext = createContext(null);
 
-// 위에서 선언한 두가지 Context 들의 Provider 로 감싸주는 컴포넌트
 export function HomeProvider({ children }) {
   const [state, dispatch] = useReducer(homeReducer, initialState);
-
   return (
     <HomeStateContext.Provider value={state}>
       <HomeDispatchContext.Provider value={dispatch}>
@@ -71,7 +64,15 @@ export function HomeProvider({ children }) {
   );
 }
 
-// State 를 쉽게 조회 할 수 있게 해주는 커스텀 Hook
+export function FurnitureProvider({ children }) {
+  const [clickIdx, setClickIdx] = useState("0");
+  return (
+    <FurnitureIdxContext.Provider value={{ clickIdx, setClickIdx }}>
+      {children}
+    </FurnitureIdxContext.Provider>
+  );
+}
+
 export function useHomeState() {
   const state = useContext(HomeStateContext);
   if (!state) {
@@ -80,13 +81,19 @@ export function useHomeState() {
   return state;
 }
 
-// Dispatch 를 쉽게 사용 할 수 있게 해주는 커스텀 Hook
 export function useHomeDispatch() {
   const dispatch = useContext(HomeDispatchContext);
   if (!dispatch) {
     throw new Error("Cannot find HomeProvider");
   }
   return dispatch;
+}
+export function useFurnitureIdx() {
+  const idx = useContext(FurnitureIdxContext);
+  if (!idx) {
+    throw new Error("Cannot find FurnitureProvider");
+  }
+  return idx;
 }
 
 export async function getHome(dispatch) {
